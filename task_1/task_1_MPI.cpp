7716280a80acbaca582dd1363441a72387b6fa46
Cr1msonEdge <91876824+Cr1msonEdge@users.noreply.github.com>
2024-02-29 12:59:20 +0300

    Add files via upload

---
+++ task_1/task_1_MPI.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include "mpi.h"
+#include <cstdio>
+
+int main(int* argc, char* argv[])
+{
+    // setlocale(LC_ALL, "rus"); проблемы при выводе в консоли
+    int numThreads, rank;  // количество потоков и ранг данного потока
+    MPI_Init(argc, &argv);
+
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &numThreads);
+
+    std::cout << "Hello world! Current number of threads is " << numThreads << ", rank: " << rank << std::endl;
+
+    MPI_Finalize();
+    // запуск программы через терминал командой:
+    // mpiexec .\task_1_MPI.exe, заранее перейдя в директорию проекта
+}
---
+++ task_1/task_1_OMP.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <omp.h>
+
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	int numberOfThreads = 2;  // количество потоков
+
+#pragma omp parallel num_threads(numberOfThreads)  
+	{
+		// участок кода, выполняемый параллельно несколькими потоками
+		// в зависимости от переменной numberOfThreads
+		std::cout << "Hello world! - поток ";
+		std::cout << omp_get_thread_num() << std::endl;
+	}
+}
+
