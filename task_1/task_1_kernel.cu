fab866382e6b2c67ab0c08383bf4638e8ef31a16
Cr1msonEdge <91876824+Cr1msonEdge@users.noreply.github.com>
2024-02-29 13:04:54 +0300

    Add files via upload

---
+++ task_1/task_1_kernel.cu
@@ -0,0 +1,30 @@
+﻿#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+#include <stdio.h>
+#include <iostream>
+
+
+__global__ void helloWorld() 
+{
+    int threadId = threadIdx.x + blockDim.x * blockIdx.x;
+    printf("Hello, World! from thread %d \n", threadId);
+    // std::cout <<  "Hello, World! из потока " << threadId << std::endl;  не работает с CUDA, надо использовать printf
+}
+
+int main() 
+{
+    // setlocale(LC_ALL, "rus");  не работает с CUDA
+    int numBlocks = 2;  // количество блоков
+    int threadsPerBlock = 3;  // потоков в блоке
+
+    // вычисляем общее количество потоков
+    int totalThreads = numBlocks * threadsPerBlock;
+
+    // вызываем ядро CUDA с заданным количеством блоков и потоков
+    helloWorld <<<numBlocks, threadsPerBlock>>>();
+
+    // ожидание завершения выполнения всех потоков
+    cudaDeviceSynchronize();
+
+    return 0;
+}
