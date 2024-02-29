b5e84ab8fc819bcf8ac15a79d547fa6ce7246e68
Cr1msonEdge <91876824+Cr1msonEdge@users.noreply.github.com>
2024-02-29 13:00:56 +0300

    Add files via upload

---
+++ task_2/task_2_MPI.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+#include "mpi.h"
+#include <stdlib.h>
+#include <time.h>
+#include <vector>
+#include <chrono>
+
+
+int main(int argc, char* argv[])
+{
+    std::srand(std::time(nullptr));
+
+    const int arraySize = 1000000;
+    unsigned int rangeOfNumbers = 100;  // максимальная граница элементов в массиве
+    int rank, size;
+    int* arr = new int[arraySize];
+    long long int partial_sum = 0;
+    long long int total_sum = 0;
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    // инициализация массива
+    for (int i = 0; i < arraySize; i++)
+    {
+        arr[i] = std::rand() % rangeOfNumbers;
+    }
+
+    auto start_time = std::chrono::high_resolution_clock::now(); // Время начала отсчета
+    // вычисление частичной суммы
+    for (int i = rank; i < arraySize; i += size) 
+    {
+        partial_sum += arr[i];
+    }
+
+    // собираем промежуточные суммы 
+    MPI_Reduce(&partial_sum, &total_sum, 1, MPI_LONG_LONG, MPI_SUM, 0, MPI_COMM_WORLD);
+
+    // время конца отсчета
+    auto end_time = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end_time - start_time);
+    
+    if (rank == 0)
+    {
+        std::cout << "Total Sum: " << total_sum << std::endl;
+        std::cout << "Time spend: " << duration.count() << " miliseconds" << std::endl;
+        std::cout << "Max number of ranks: " << size << std::endl;
+    }
+
+    MPI_Finalize();
+    delete[] arr;
+    return 0;
+}
---
+++ task_2/task_2_OMP.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+#include <omp.h>
+#include <time.h>
+#include <stdlib.h>
+#include <chrono>
+
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+
+    int arraySize = 1000000;  // размер массива
+    
+    // генерация массива с произвольными элементами
+    std::srand(std::time(nullptr));
+    int* arr = new int[arraySize];
+    unsigned int rangeOfNumbers = 100;  // максимальная граница элементов в массиве
+
+    for (size_t i = 0; i < arraySize; ++i)
+    {
+        arr[i] = std::rand() % rangeOfNumbers;
+    }
+    // вывод элементов массива
+    /*for (size_t i = 0; i < arraySize; ++i)
+    {
+        std::cout << arr[i] << ' ';
+    }
+    std::cout << std::endl;*/
+
+    int result = 0;  // итоговая сумма
+    auto start_time = std::chrono::high_resolution_clock::now(); // Время начала отсчета
+
+    // Запуск цикла, который вычисляет сумму параллельно. 
+    // reduction(+:result) означает, что значение переменной result будет зависеть от всех потоков, т.е.
+    // итоговая сумма будет "собираться" из промежуточных сумм всех потоков 
+#pragma omp parallel for reduction(+:result)  
+    for (int i = 0; i < arraySize; ++i)
+    {
+        result += arr[i];
+    }
+
+    // время конца отсчета
+    auto end_time = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end_time - start_time);
+    std::cout << "Итоговая сумма: " << result << std::endl;
+    std::cout << "Затраченное время: " << duration.count() << " мс." << std::endl;
+    std::cout << "Количество потоков: " << omp_get_max_threads() << std::endl;
+    delete[] arr;
+}
